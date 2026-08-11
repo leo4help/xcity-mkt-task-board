ab464486dd37ec33a900e49290c418ea5ed94be8
leo4help <leo4help@gmail.com>
2026-08-11 20:23:49 -0300

    Add config.js for GitHub Pages deployment

---
+++ config.js
@@ -0,0 +1,11 @@
+window.XCITY_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbw6ZrQ0fG0SenVmVrR_5S7-90N3UqSEFP-cxBYSvysxxQDHdGjxNrq-ajDllcPqPA4i/exec",
+  API_TOKEN: "xcity-marketing-k3n8p2x7qz",
+
+  // 目前先關閉密碼鎖，方便快速查看；之後要恢復只要改成 true，並確認 PASSWORD 有設定
+  REQUIRE_PASSWORD: false,
+  PASSWORD: "888xcity",
+
+  SHEET_URL: "https://docs.google.com/spreadsheets/d/18eCWSNBphjyL8z11SXll-Qi7IVLUtwhui2BtEoXh4D8",
+  PROJECTS: ["社群維運", "會員制度", "MaisonX宣傳", "大人國"]
+};
